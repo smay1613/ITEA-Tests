baf1192e58c77be8b3008f65873204175cb34eff
Dmytro Afanasiev <DAfanasiev@luxoft.com>
2019-08-05 16:14:33 +0300

    Cleanup

--- include/Utils.h
+++ include/Utils.h
@@ -2,18 +2,6 @@
 #include <string>
 #include <chrono>
 
-namespace TagLib
-{
-namespace FLAC {class File;};
-namespace APE {class Tag; class File;};
-namespace ID3v2 {class Tag;};
-namespace ASF {class File;};
-namespace MP4 {class File;};
-namespace MPEG {class File;};
-namespace MPC {class File;};
-namespace RIFF {namespace WAV {class File;}}
-}
-
 namespace avprocessing
 {
 namespace utilities
