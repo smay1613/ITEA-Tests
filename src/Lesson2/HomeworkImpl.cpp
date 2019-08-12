b1e347542e8bdb492b789c10978dfb4f0e117c0e
Dmytro Afanasiev <DAfanasiev@luxoft.com>
2019-08-12 13:59:09 +0300

    Inserts implementation

--- src/Lesson2/HomeworkImpl.cpp
+++ src/Lesson2/HomeworkImpl.cpp
@@ -20,88 +20,35 @@ struct node
 
 void linkLevelNodes(node* root)
 {
-    std::queue<node *> firstLevel;
-    firstLevel.push(root);
-    while (!firstLevel.empty())
-    {
-        std::queue<node *> secondLevel;
-        if (firstLevel.front()->left != nullptr)
-        {
-            secondLevel.push(firstLevel.front()->left);
-        }
-        if (firstLevel.front()->right != nullptr)
-        {
-            secondLevel.push(firstLevel.front()->right);
-        }
-        node* tmp = firstLevel.front();
-        firstLevel.pop();
-        while (!firstLevel.empty())
-        {
-            tmp->level = firstLevel.front();
-            tmp = tmp->level;
-            if (tmp->left != nullptr)
-            {
-                secondLevel.push(tmp->left);
-            }
-            if (tmp->right != nullptr)
-            {
-                secondLevel.push(tmp->right);
-            }
-            firstLevel.pop();
-        }
-        tmp->level = nullptr;
-        firstLevel = secondLevel;
-    }
-}
 
-char reverseRightToLeftBracketsFunction(const char m_rightBrackets){
-    switch (m_rightBrackets)
-    {
-    case '}':
-    {
-        return '{';
-    }
-    case ')':
-    {
-        return '(';
-    }
-    case ']':
-    {
-        return '[';
-    }
-    default:
-        return NULL;
-    }
 }
 
 bool isExpressionValid(const std::string &expression)
 {
-    std::stack<char>stackOpenBrackets;
-        for(const auto &value: expression){
-            if(value == '{' || value == '(' || value == '[')
+    std::stack <char> bracecStack{};
+
+    for(const auto &brace : expression)
+    {
+        if(brace == '(' || brace == '{' || brace == '[')
+        {
+            bracecStack.push(brace);
+        }
+
+        if(bracecStack.empty())
+        {
+            return false;
+        }
+
+        if(!bracecStack.empty())
+        {
+            if((brace == ')' && bracecStack.top() == '(')
+               || (brace == '}' && bracecStack.top() == '{')
+               || (brace == ']' && bracecStack.top() == '['))
             {
-                stackOpenBrackets.push(value);
-            }
-            else {
-                if(stackOpenBrackets.empty()){
-                    return false;
-                }
-
-                char reverseRightToLeftBrackets{reverseRightToLeftBracketsFunction(value)};
-
-                if(stackOpenBrackets.top() != reverseRightToLeftBrackets )
-                {
-                    return false;
-                }
-                else
-                {
-                    stackOpenBrackets.pop();
-                }
+                bracecStack.pop();
             }
         }
-        if(stackOpenBrackets.empty()){
-            return true;
-        }
-        return false;
+    }
+    return bracecStack.empty();
 }
 
