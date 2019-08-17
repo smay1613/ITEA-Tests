d2f8b0e55e14b494e7b31b47d573986931da462f
smay1613 <dimaafa0@gmail.com>
2019-08-17 20:42:34 +0300

    Inserts imp test3

--- src/Lesson3/HomeworkImpl.cpp
+++ src/Lesson3/HomeworkImpl.cpp
@@ -5,31 +5,61 @@
 #include <sstream>
 
 std::vector<std::string> getUniqueWords(const std::string & text) {
-    std::string word {};
-    std::stringstream SString {text};
-    std::set<std::string> resultSet;
+    std::vector<std::string> words_vector;
+        std::set<std::string> words_set;
+        std::string word;
+        std::stringstream str_stream;
 
-    while (!SString.eof())
-    {
-        SString >> word;
-        resultSet.insert(word);
-    }
+        str_stream << text;
+        while (!str_stream.eof())
+        {
+            str_stream >> word;
+            words_set.insert(word);
+        }
 
-    std::vector<std::string> resultVector {resultSet.begin(), resultSet.end()};
-    return resultVector;
+        for (const auto& element : words_set)
+        {
+            words_vector.push_back(element);
+        }
+
+        return words_vector;
 }
 
 std::vector<std::pair<std::string, size_t>> wordCounter(const std::string & text) {
-    std::string word {};
-    std::stringstream SString {text};
-    std::map<std::string, size_t> resultMap;
+    std::vector<std::pair<std::string, size_t>> words_vector;
+        std::map<std::string, size_t> words_map;
+        std::string word;
+        std::stringstream str_stream;
 
-    while (!SString.eof())
-    {
-        SString >> word;
-        ++resultMap[word] ;
-    }
+        str_stream << text;
+        while (!str_stream.eof())
+        {
+            str_stream >> word;
+            if (words_map.empty())
+            {
+                words_map.insert(std::make_pair(word, 1));
+                word.clear();
+            }
+            else
+            {
+                std::map<std::string, size_t>::iterator element = words_map.find(word);
 
-    std::vector<std::pair<std::string, size_t>> resultVector {resultMap.begin(), resultMap.end()};
-    return resultVector;
+                if (element != words_map.end())
+                {
+                    element->second++;
+                    word.clear();
+                }
+                else {
+                    words_map.insert(std::make_pair(word, 1));
+                    word.clear();
+                }
+            }
+        }
+
+        for (const auto & element : words_map)
+        {
+            words_vector.push_back(element);
+        }
+
+        return words_vector;
 }
