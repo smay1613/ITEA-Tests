79537536d0c340c859f76107aea9d919f3d0b950
smay1613 <dimaafa0@gmail.com>
2019-09-26 14:35:34 +0300

    Inserts first homework

--- src/Lesson1/HomeworkImpl.cpp
+++ src/Lesson1/HomeworkImpl.cpp
@@ -5,58 +5,38 @@
 
 std::vector<int> combineVectors (const std::vector<int>::iterator& begin1, const std::vector<int>::iterator& end1, const std::vector<int>::iterator& begin2, const std::vector<int>::iterator& end2)
 {
-    std::vector<int> resultVector;
-    resultVector.reserve((std::distance(begin1,end1)+std::distance(begin2,end2)));
-    resultVector.insert(resultVector.end(), begin1, end1);
-    resultVector.insert(resultVector.end(), begin2, end2);
-    return resultVector;
+    std::vector<int> vector_result {};
+    vector_result.insert(vector_result.end(), std::make_move_iterator(begin1), std::make_move_iterator(end1));
+    vector_result.insert(vector_result.end(), std::make_move_iterator(begin2), std::make_move_iterator(end2));
+    return vector_result;
 };
 
 size_t magicNumber(const size_t number, const size_t degree)
 {
-    std::vector<size_t> VectorOfDig;
-        size_t resultDigit = 0;
-        std::list<size_t> resultList;
-        VectorOfDig.reserve(degree);
-        for(size_t i = 0; i < degree; i++)
-        {
-            if(i == 0)
-            {
-                VectorOfDig.push_back(degree);
-            }
-            else
-            {   size_t element = (*(VectorOfDig.end()-1))*degree;
-                VectorOfDig.push_back(element);
-            }
-        }
-        for(auto number : VectorOfDig)
-        {
-            std::list<size_t> ListOfDig;
-            while (number > 0)
-            {
-                size_t digit = number%10;
-                ListOfDig.push_back(digit);
-                number /= 10;
-            }
-            resultList.push_back(ListOfDig.back());
-            resultList.push_back(ListOfDig.front());
+    std::list<int> list {};
 
+    for (size_t i = 1; i <= degree; ++i) {
+        list.push_back(static_cast<int>(std::pow(number,i)));
+    }
+    ulong num = 0;
+    for (std::list<int>::iterator it = list.begin(); it != list.end(); ++it) {
+        uint32_t temp_for_num = *it % 10;
+        while (*it != 0) {
+            *it /= 10;
+            if (*it >= 1 && *it <= 9) {
+                num = (num * 10) + *it % 10;
+                num = (num * 10) + temp_for_num;
+            }
         }
-        size_t count{1};
-        for( std::list<size_t>::reverse_iterator it = resultList.rbegin(); it!= resultList.rend(); it++)
-        {
-           resultDigit += (*it)*count;
-           count *= 10;
-        }
-       return resultDigit;
+    }
+    return num;
 };
 
 void reverse(std::forward_list<int>& listToReverse)
 {
-    std::forward_list<int> reverseList{};
-    for(auto& element : listToReverse)
-    {
-        reverseList.push_front(element);
+    std::forward_list<int> modifiedListToReverse {};
+
+    for (std::forward_list<int>::iterator it = listToReverse.begin(); it != listToReverse.end(); ++it) {
+        modifiedListToReverse.insert_after(modifiedListToReverse.before_begin(), *it);
     }
-    listToReverse.swap(reverseList);
 };
