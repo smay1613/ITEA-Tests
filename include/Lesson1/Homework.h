6bdd417967327a2403f6dcc263c5b6573b5db953
smay1613 <dimaafa0@gmail.com>
2019-09-26 13:37:55 +0300

    Cleanup

--- include/Lesson1/Homework.h
+++ include/Lesson1/Homework.h
@@ -1,11 +1,1 @@
 #pragma once
-#include <string>
-#include <vector>
-#include <forward_list>
-
-std::vector<int> combineVectors(const std::vector<int>::iterator firstVectorBeginIterator, const std::vector<int>::iterator firstVectorEndIterator,
-                                const std::vector<int>::iterator secondVectorBeginIterator, const std::vector<int>::iterator secondVectorEndIterator);
-
-size_t magicNumber(const size_t number, const size_t degree);
-
-void reverse(std::forward_list<int>& listToReverse);
