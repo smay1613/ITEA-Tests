b8cbd9cb51befb281e65e3dfbc1af172c5bd9f12
Dmytro Afanasiev <DAfanasiev@luxoft.com>
2019-10-03 18:05:33 +0300

    Inserts implementation

--- src/Lesson2/HomeworkImpl.cpp
+++ src/Lesson2/HomeworkImpl.cpp
@@ -2,6 +2,7 @@
 #include <stack>
 #include <queue>
 #include <map>
+#include <cmath>
 
 int getNodeLevel (node* nodeToObserve, node* nodeTofind, int level)
 {
@@ -27,68 +28,64 @@ int getNodeLevel (node* nodeToObserve, node* nodeTofind, int level)
 
 void linkLevelNodes(node* root)
 {
-    if(root == nullptr)
-    {
-        return;
-    }
+    if (nullptr == root) {
+       return;
+     }
+     std::queue<node*> ptrNode;
+     ptrNode.push(root);
+     size_t NextLevelNodes {0};
+     size_t expectedNodes {2};
+     bool isPointToNext = true;
+     node* previusNodePtr = nullptr;
+     while (!ptrNode.empty()) {
+       node& currentNode = *(ptrNode.front());
+       if (isPointToNext) {
+         currentNode.level = previusNodePtr;
+       }
+       isPointToNext = true;
+       if (currentNode.right != nullptr) {
+         ptrNode.push(currentNode.right);
+         ++NextLevelNodes;
+       } else {
+         --expectedNodes;
+       }
+       if (currentNode.left != nullptr) {
+       ptrNode.push(currentNode.left);
+         ++NextLevelNodes;
+       } else {
+         --expectedNodes;
+       }
+       if (expectedNodes == NextLevelNodes) {
+         expectedNodes = pow(2, expectedNodes);
+         NextLevelNodes = 0;
+         isPointToNext = false;
+       }
+       previusNodePtr = &currentNode;
+       ptrNode.pop();
+     }
+}
 
-    std::queue<node*> discoveredNodes;
-    discoveredNodes.push(root);
-
-    while(!discoveredNodes.empty())
-    {
-        node* current_node = discoveredNodes.front();
-        discoveredNodes.pop();
-        node* next_node = nullptr;
-        if(!discoveredNodes.empty())
-        {
-            next_node = discoveredNodes.front();
-
-            int nodeLevelDifference = getNodeLevel(root, current_node, 0) - getNodeLevel(root, next_node, 0);
-
-            if(nodeLevelDifference == 0)
-            {
-                current_node->level = next_node;
-            }
-        }
-
-        if (current_node->left != nullptr)
-        {
-            discoveredNodes.push(current_node->left);
-        }
-
-        if (current_node->right != nullptr)
-        {
-            discoveredNodes.push(current_node->right);
-        }
-        }
+inline char OpositBracket(const char BRACKET) {
+  std::map<char, char> bracketsMap {{'(',')'},{'[',']'},{'{','}'}};
+  return bracketsMap.at(BRACKET);
 }
 
 bool isExpressionValid(const std::string &expression)
 {
-    std::stack<char> receivedMessage;
-    std::map<char, char> openBracketsMap {{'(', ')'}, {'[', ']'}, {'{', '}'}};
-    std::map<char, char> closedBracketsMap {{')', '('}, {']', '['}, {'}', '{'}};
-
-    for(const auto& element : expression)
-    {
-        if(openBracketsMap.find(element) != openBracketsMap.end())
-        {
-            receivedMessage.push(element);
+    std::stack<char> bracketsStack;
+      for (char bracket : expression) {
+        if (bracket == '(' ||
+            bracket == '[' ||
+            bracket == '{') {
+          bracketsStack.push(bracket);
+        } else {
+          if (bracketsStack.empty()) {return false;}
+          if (bracket != OpositBracket(bracketsStack.top())) {
+            return false;
+          }
+          bracketsStack.pop();
         }
-        else
-        {
-            if (!receivedMessage.empty() && closedBracketsMap.find(element)->second == receivedMessage.top())
-            {
-                receivedMessage.pop();
-            }
-            else
-            {
-                return false;
-            }
-        }
-    }
-
-    return receivedMessage.empty();
+      }
+      return bracketsStack.empty();
 }
 
