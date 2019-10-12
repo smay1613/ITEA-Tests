52125140172cd8523d8893380f90b86e4b202f53
smay1613 <dimaafa0@gmail.com>
2019-10-12 16:10:07 +0300

    Inserts test3

--- src/Lesson3/HomeworkImpl.cpp
+++ src/Lesson3/HomeworkImpl.cpp
@@ -3,63 +3,49 @@
 #include <map>
 #include <algorithm>
 #include <sstream>
+#include <map>
+#include <set>
+#include <sstream>
+#include <vector>
+#include <functional>
 
-std::vector<std::string> getUniqueWords(const std::string & text) {
-    std::vector<std::string> words_vector;
-        std::set<std::string> words_set;
-        std::string word;
-        std::stringstream str_stream;
 
-        str_stream << text;
-        while (!str_stream.eof())
-        {
-            str_stream >> word;
-            words_set.insert(word);
-        }
-
-        for (const auto& element : words_set)
-        {
-            words_vector.push_back(element);
-        }
-
-        return words_vector;
+std::string transformToLowerCase (std::string stringToTransform) {
+  std::transform (stringToTransform.begin(), stringToTransform.end(), stringToTransform.begin(), ::tolower);
+  return stringToTransform;
 }
 
-std::vector<std::pair<std::string, size_t>> wordCounter(const std::string & text) {
-    std::vector<std::pair<std::string, size_t>> words_vector;
-        std::map<std::string, size_t> words_map;
-        std::string word;
-        std::stringstream str_stream;
+std::vector<std::string> getUniqueWords (const std::string& text) {
+  std::set<std::string> mySet {};
+  std::stringstream streamText {text};
+  std::string temp_string {};
 
-        str_stream << text;
-        while (!str_stream.eof())
-        {
-            str_stream >> word;
-            if (words_map.empty())
-            {
-                words_map.insert(std::make_pair(word, 1));
-                word.clear();
-            }
-            else
-            {
-                std::map<std::string, size_t>::iterator element = words_map.find(word);
+  while (streamText >> temp_string) {
+      temp_string.erase(std::remove_if(temp_string.begin(), temp_string.end(), ispunct), temp_string.end());
+      mySet.insert(transformToLowerCase(temp_string));
+    }
 
-                if (element != words_map.end())
-                {
-                    element->second++;
-                    word.clear();
-                }
-                else {
-                    words_map.insert(std::make_pair(word, 1));
-                    word.clear();
-                }
-            }
+  std::vector<std::string> uniqueWords {mySet.begin(), mySet.end()};
+
+  return uniqueWords;
+}
+
+std::vector<std::pair<std::string, size_t>> wordCounter (const std::string& text) {
+  std::map<std::string, size_t> myMap {};
+  std::stringstream streamText {text};
+  std::string temp_string {};
+
+  while (streamText >> temp_string) {
+      temp_string.erase(std::remove_if(temp_string.begin(), temp_string.end(), ispunct), temp_string.end());
+      std::map<std::string, size_t>::iterator search {myMap.find(transformToLowerCase(temp_string))};
+
+      if (search == myMap.end()) {
+          myMap.emplace(transformToLowerCase(temp_string), 1);
+        } else {
+          ++search->second;
         }
+    }
 
-        for (const auto & element : words_map)
-        {
-            words_vector.push_back(element);
-        }
-
-        return words_vector;
+  std::vector<std::pair<std::string, size_t>> uniqueWords {myMap.begin(), myMap.end()};
+  return uniqueWords;
 }
